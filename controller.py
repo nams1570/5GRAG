9ca634b9106c0ba9e89d483affbef3a903602ee7
nams1570 <amanganapathy@gmail.com>
2024-05-13 18:45:34 -0700

    Updated to include multi-doc rag; issues with tesseract.

--- controller.py
+++ controller.py
@@ -3,17 +3,20 @@ from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser #converts output into string
 from langchain_core.prompts import ChatPromptTemplate 
 from langchain_openai import OpenAIEmbeddings
-from langchain_community.vectorstores import FAISS #simple local vector store
+from langchain_chroma import Chroma
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
 from langchain_community.document_loaders import WebBaseLoader
-
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import UnstructuredFileLoader
+import pytesseract
+import os
 
 
 API_KEY = config["API_KEY"]
 M_NAME = config["MODEL_NAME"]
-
+DOC_DIR = config["DOC_DIR"]
 
 class Controller:
     def __init__(self):
@@ -27,17 +30,26 @@ Question: {input}""")
         self.docs = None
 
     def updateDocs(self):
-        loader = WebBaseLoader("https://docs.smith.langchain.com/user_guide")
-        self.docs = loader.load()
+        #We need a separate loader for each document. 
+        loaders = [UnstructuredFileLoader(os.path.join(DOC_DIR,fn),mode='elements') for fn in os.listdir(DOC_DIR)]
+        print(loaders)
+        self.docs = []
+
+        for loader in loaders:
+            print(f"Document is {loader.file_path}")
+            print(os.path.exists(loader.file_path))
+            raw_doc = loader.load()
+            print(raw_doc[:5])
+            text_splitter = RecursiveCharacterTextSplitter() #creates a text splitter, which breaks apart the document into text
+            doc = text_splitter.split_documents(raw_doc) #applies the text splitter to the documents
+            self.docs += [doc]
 
     def createVectorStore(self):
         """ create the vector database which will store the vector embeddings of the\
               documents that will be retrieved."""
         embeddings = OpenAIEmbeddings(model='text-embedding-3-small',api_key=API_KEY) #Since we're using openAI's llm, we have to use its embedding model
         self.updateDocs()
-        text_splitter = RecursiveCharacterTextSplitter() #creates a text splitter, which breaks apart the document into text
-        documents = text_splitter.split_documents(self.docs) #applies the text splitter to the documents
-        vector = FAISS.from_documents(documents, embeddings) 
+        vector = Chroma.from_documents(self.docs, embeddings) 
         self.retriever = vector.as_retriever()
         
     def runController(self):
