498442846b6c2288459001b7e70ca6e960c8d944
nams1570 <amanganapathy@gmail.com>
2024-05-21 13:19:21 -0700

    Adjusted prompt to make it less strict of a use for the db, and wrote the toggle function.

--- controller.py
+++ controller.py
@@ -25,13 +25,14 @@ class Controller:
     def __init__(self):
         self.output_parser = StrOutputParser()
         self.llm = ChatOpenAI(api_key = API_KEY, model=M_NAME)
-        self.prompt = ChatPromptTemplate.from_template("""Answer the following question based only on the provided context:
+        self.prompt = ChatPromptTemplate.from_template("""Answer the following question with reference to the provided context:
 <context>
 {context}
 </context>
 Question: {input}""")
         self.docs = None
         self.isCreated = False
+        self.isDatabaseTriggered = True
 
     def updateDocs(self):
         #We need a separate loader for each document. 
@@ -45,6 +46,10 @@ Question: {input}""")
             doc = text_splitter.split_documents(raw_doc) #applies the text splitter to the documents
             self.docs.extend(doc)
 
+    def toggleDatabase(self):
+        self.isDatabaseTriggered = not self.isDatabaseTriggered
+        print(f"flag is {self.isDatabaseTriggered}")
+
     def createVectorStore(self):
         """ create the vector database which will store the vector embeddings of the\
               documents that will be retrieved."""
@@ -57,12 +62,15 @@ Question: {input}""")
 ) #express query in multiple ways to improve hit rate
         
         self.isCreated = True
+
     def convert_history(self, history):
         message_objects = []
         for turn in history:
             message_objects.append(HumanMessage(content=turn[0]))
             message_objects.append(AIMessage(content=turn[1]))
         return message_objects
+    
+
     def runController(self, prompt, history):
         if not self.isCreated:
             self.createVectorStore()
@@ -71,11 +79,14 @@ Question: {input}""")
             #doc_chain is a chain that lets you pass a document to the llm and it uses that to answer
             doc_chain = create_stuff_documents_chain(self.llm, self.prompt)
             # retrieval chain passed the load of deciding what document to use to answer to the retriever.
-            retrieval_chain = create_retrieval_chain(self.retriever, doc_chain)
             history = self.convert_history(history)
-            resp = retrieval_chain.invoke({"input":prompt,"history": history})
+            if self.isDatabaseTriggered:
+                retrieval_chain = create_retrieval_chain(self.retriever, doc_chain)
+    
+                resp = retrieval_chain.invoke({"input":prompt,"history": history})
+            else:
+                resp = doc_chain.invoke({"input":prompt,"history": history})
             response = resp['answer']
-            #print(f"response is {response}")
             return response
     
 if __name__ == "__main__":
