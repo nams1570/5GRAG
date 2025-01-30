94772cdfd94b74a8fd47ba3b2e3fed28f7a647fc
nams1570 <amanganapathy@gmail.com>
2025-01-30 15:17:05 -0800

    Updated downloadFile method to avoid conflicts, setup run loop

--- AutoFetcher.py
+++ AutoFetcher.py
@@ -2,6 +2,7 @@ from settings import config
 import requests
 from bs4 import BeautifulSoup
 import os
+import random
 # idea:
 # These pages are maintained quite consistently. 
 # two questions:
@@ -11,7 +12,7 @@ import os
 class AutoFetcher:
     def __init__(self,fetch_endpoints):
         self.links = {}
-        pass
+        self.fetch_endpoints = fetch_endpoints
 
     def extractLinksFromEndpoint(self,endpoint,params):
         """For a specific endpoint we retrieve the page and collect all the links on that page. 
@@ -27,26 +28,35 @@ class AutoFetcher:
             self.links[endpoint].append(link.get('href'))
     
     def getMostRecentLink(self,endpoint):
+        """Current assumption is that the last link is the most recent one"""
         if not self.links.get(endpoint,[]):
             raise Exception("Error: Run extractLinksFromEndpoint first")
         return self.links[endpoint][-1]
     
-    def getFileFromLink(self,link):
+    def downloadFileFromLink(self,link):
+        """Makes the assumption that the last part of the address given is the name of the file to the downloaded
+        Note: currently set up to work with duplicates. If the filename already exists, it will append a random hash to the end of the filename and then try to download.
+        @link: http[s] endpoint where a file can be downloaded with a get request"""
         filename = link.split("/")[-1]
         filepath = os.path.join(config['DOC_DIR'],filename)
+        while os.path.exists(filepath):
+            filepath = os.path.join(config['DOC_DIR'],filename+f"{random.randint(0,65535)}")
 
         response = requests.get(link)
         if response.status_code != 200:
             raise Exception("Error: Could not retrieve page content")
-        
+
         with open(filepath,'wb') as file:
             file.write(response.content)
 
+    def run(self,params):
+        for endpoint in self.fetch_endpoints:
+            self.extractLinksFromEndpoint(endpoint,params)
+            link = self.getMostRecentLink(endpoint)
+            self.downloadFileFromLink(link)
+
 if __name__ == "__main__":
     params = {"sortby":"date"}
-    endpoint = "https://www.3gpp.org/ftp/Specs/latest/Rel-16/38_series"
-    af = AutoFetcher([endpoint])
-    af.extractLinksFromEndpoint(endpoint,params)
-    link =af.getMostRecentLink(endpoint)
-    print(f"link is {link}")
-    af.getFileFromLink(link)
+    endpoints = ["https://www.3gpp.org/ftp/Specs/latest/Rel-16/38_series","https://www.3gpp.org/ftp/Specs/latest/Rel-17/38_series","https://www.3gpp.org/ftp/Specs/latest/Rel-18/38_series"]
+    af = AutoFetcher(endpoints)
+    af.run(params)
