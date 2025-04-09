c1bb2cb4662a8af3c464e12169f1c74a609806f6
nams1570 <amanganapathy@gmail.com>
2025-04-09 13:33:14 -0700

    Test functionality to extract all relevant data from doc
    docx.Document.paragraphs does not capture table data.

--- ClauseExtractor.py
+++ ClauseExtractor.py
@@ -5,14 +5,25 @@ FILENAME = "./data/38214-hc0.docx"
 f = open(FILENAME,'rb')
 doc = Document(f)
 
-p = doc.add_paragraph('This is a paragraph ')
-
 def iter_readings(paragraphs):
     for paragraph in paragraphs:
         if paragraph.style.name.startswith('Heading'):
             yield paragraph
 
+def iter_paragraphs(doc):
+    for paragraph in doc.paragraphs:
+        yield paragraph.text
+
+def iter_sections(doc):
+    for part in doc.iter_inner_content():
+        if part.style.name.startswith('Heading'):
+            yield "hd: "+part.text
+        elif "table" in part.style.name.lower():
+            yield "tbl: " + part.style.name
+        else:
+            yield part.text
+
 
 if __name__ == "__main__":
-    for heading in iter_readings(doc.paragraphs):
-        print(heading.text)
+    for heading in iter_sections(doc):
+        print(heading)
