ada5156d10003a916a524eee0c948c648ad21547
nams1570 <amanganapathy@gmail.com>
2025-04-10 13:38:20 -0700

    Build section based chunking ability with inchoate table parsing

--- ClauseExtractor.py
+++ ClauseExtractor.py
@@ -1,6 +1,6 @@
 from docx import Document
 
-FILENAME = "./data/38214-hc0.docx"
+FILENAME = "./data/TestFile.docx"
 
 f = open(FILENAME,'rb')
 doc = Document(f)
@@ -14,12 +14,21 @@ def iter_paragraphs(doc):
     for paragraph in doc.paragraphs:
         yield paragraph.text
 
+def parse_table(table):
+    for row_idx,row in enumerate(table.rows):
+        for col_idx,col in enumerate(row.cells):
+            if col.tables:
+                for nested_table in col.tables:
+                    parse_table(nested_table)
+            else:
+                yield col.text
+
 def iter_sections(doc):
     for part in doc.iter_inner_content():
         if part.style.name.startswith('Heading'):
             yield "hd: "+part.text
         elif "table" in part.style.name.lower():
-            yield "tbl: " + part.style.name
+            yield "tbl: " + f"{[val for val in parse_table(part)]}"
         else:
             yield part.text
 
--- MetadataAwareChunker.py
+++ MetadataAwareChunker.py
@@ -3,6 +3,18 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
 
 
+def parse_table(table):
+    """@input: docx table. Consists of rows and cells and the cells may have other tables.
+    This is a helper function to help parse tables from docx. 
+    It is needed as tables may contain nested tables."""
+    for row_idx,row in enumerate(table.rows):
+        for col_idx,col in enumerate(row.cells):
+            if col.tables:
+                for nested_table in col.tables:
+                    parse_table(nested_table)
+            else:
+                yield col.text
+
 def getSectionedChunks(file_list):
     """@input: file_list. List of files in relative path that will be chunked.
     Returns: master list chunks_with_metadata that has chunks of all the files stored as langchain Documents.
@@ -15,25 +27,36 @@ def getSectionedChunks(file_list):
 
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200,add_start_index=True)
 
-        current_section = "N/A"
+        current_section_title = "N/A"
+        current_section_text = ""
         sections = []
         #TODO: Switch to collecting a "current section" list of text chunks
         # currently there's no overlap between paragraphs in the same section which is bad for retrieval
         # We want to collect (section_text,current_section) where section_text is a combination of paragraphs
         # Then, we use the text_splitter to split it into split chunks
-        for paragraph in doc.paragraphs:
-            text = paragraph.text.strip()
-            if paragraph.style.name.startswith('Heading'):
-                current_section = paragraph.text
-            elif text:
-                sections.append((text,current_section))
-            
-        for text,section in sections:
+        for part in doc.iter_inner_content():
+            if part.style.name.startswith('Heading'):
+                # update current section
+                sections.append((current_section_text,current_section_title))
+                current_section_text = ""
+                current_section_title = part.text
+            elif "table" in part.style.name.lower():
+                print(f"*******HERE IS TABLE*********\n")
+                for table_datum in parse_table(part):
+                    current_section_text += table_datum
+            else:
+                # append text to current section
+                current_section_text += part.text
+        
+        #Add last section to sections
+        sections.append((current_section_text,current_section_title))
+
+        for text,section_name in sections:
             split_chunks = text_splitter.split_text(text)
             for chunk in split_chunks:
                 chunks_with_metadata.append(Document(
                     page_content=chunk,
-                    metadata={'source':file,'section':section}
+                    metadata={'source':file,'section':section_name}
                 ))
     return chunks_with_metadata
 
@@ -57,5 +80,5 @@ def getFullFileChunks(file_list):
     return chunks
 
 if __name__ =="__main__":
-    file_list = ["./data/38214-hc0.docx"]
-    print(getSectionedChunks(file_list)[-100])
+    file_list = ["./data/TestFile.docx"]
+    print(getSectionedChunks(file_list))
