fc12ff6535269955cb480ca9c2003555a194f323
nams1570 <amanganapathy@gmail.com>
2025-04-17 13:52:42 -0700

    Refactor retrieval into new class

---
+++ MultiStageRetriever.py
@@ -0,0 +1,37 @@
+from langchain.retrievers.multi_query import MultiQueryRetriever
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+
+class MultiStageRetriever:
+    def __init__(self,llm,prompt_template):
+        self.llm = llm
+        self.doc_chain = create_stuff_documents_chain(self.llm, prompt_template)
+
+    def reconstructDocChain(self,new_prompt_template):
+        self.doc_chain = create_stuff_documents_chain(self.llm,new_prompt_template)
+
+    def constructRetriever(self,db,selected_docs=None):
+        """@selected_docs: list of documents to filter.
+        @db: DBClient instance.
+        @llm: the llm used for rewriting queries
+        returns: Nothing"""
+        if selected_docs is None or len(selected_docs) == 0:
+            self.base_retriever = MultiQueryRetriever.from_llm(
+                            retriever=db.getRetriever(), llm=self.llm
+                        ) 
+        else:
+            # If we have selected one or more docs, then apply filtering
+            name_list = [os.path.join(DOC_DIR,doc) for doc in selected_docs]
+            print("name_list: ", name_list)
+            name_filter = {"source": {"$in": name_list}}
+            self.base_retriever = MultiQueryRetriever.from_llm(
+                            retriever=db.getRetriever(search_kwargs={'filter': name_filter}), llm=self.llm
+                        )
+    def invoke(self,query,history):
+        if not self.base_retriever:
+            raise Exception("Error: No base retriever initialized. Has constructRetriever been run?")
+        retrieval_chain = create_retrieval_chain(self.base_retriever, self.doc_chain)
+        resp = retrieval_chain.invoke({"input":query,"history":history})
+        return resp
+    
+    
--- controller.py
+++ controller.py
@@ -8,10 +8,7 @@ from langchain_core.output_parsers import StrOutputParser #converts output into 
 from langchain_core.prompts import ChatPromptTemplate 
 from langchain_openai import OpenAIEmbeddings
 from langchain_core.messages import HumanMessage, AIMessage
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains import create_retrieval_chain
-from langchain.retrievers.multi_query import MultiQueryRetriever
-
+from MultiStageRetriever import MultiStageRetriever
 import os
 
 API_KEY = config["API_KEY"]
@@ -37,6 +34,8 @@ Question: {input}""")
         #endpoints += ["https://www.3gpp.org/ftp/Specs/latest/Rel-18/38_series"]
         self.params = params = {"sortby":"date"}
         self.af = AutoFetcher(endpoints,unzipFile)
+
+        self.retriever = MultiStageRetriever(llm=self.llm,prompt_template = self.prompt)
 
         self.isDatabaseTriggered = True
 
@@ -64,6 +63,7 @@ Question: {input}""")
 Question: {input}""")
         else:
             self.prompt = ChatPromptTemplate.from_template("""Answer the following question as best you can Question: {input}""")
+        self.retriever.reconstructDocChain(self.prompt)
         return self.isDatabaseTriggered
 
     def convert_history(self, history):
@@ -75,31 +75,15 @@ Question: {input}""")
         return message_objects
     
 
-    def constructRetriever(self,selected_docs):
-        if selected_docs is None or len(selected_docs) == 0:
-            return MultiQueryRetriever.from_llm(
-                            retriever=self.db.getRetriever(), llm=self.llm
-                        ) 
-        else:
-            # If we have selected one or more docs, then apply filtering
-            name_list = [os.path.join(DOC_DIR,doc) for doc in selected_docs]
-            print("name_list: ", name_list)
-            name_filter = {"source": {"$in": name_list}}
-            return MultiQueryRetriever.from_llm(
-                            retriever=self.db.getRetriever(search_kwargs={'filter': name_filter}), llm=self.llm
-                        )            
-
     def getResponseWithRetrieval(self,prompt,history):
-        doc_chain = create_stuff_documents_chain(self.llm, self.prompt)
-        retrieval_chain = create_retrieval_chain(self.retriever, doc_chain)
+        
     
         #retriever.invoke gives us docs
-        retrieved_docs = self.retriever.invoke(prompt)
+        #retrieved_docs = self.retriever.get_relevant_documents(prompt)
 
-        print(f"\n retrieved docs are {retrieved_docs}\n")
-
-        #resp = retrieval_chain.invoke({"input":prompt,"history": history})
-        resp_answer = doc_chain.invoke({"context":retrieved_docs,"input":prompt})
+        #print(f"\n retrieved docs are {retrieved_docs}\n")
+        resp = self.retriever.invoke(query=prompt,history=history)
+        #resp_answer = doc_chain.invoke({"context":retrieved_docs,"input":prompt})
         """all_docs = resp['context'][:]
         ext_src: list[RefObj] = RExt.runREWithDocList(docs=all_docs)
         #print(f"ext_src is {ext_src[0].reference}")
@@ -107,13 +91,13 @@ Question: {input}""")
             res = self.retriever.invoke(f"You are an expert retriever with access to a vector database. Parse through the database, and only return data from this section: {refObj.reference}")
             print(f"for ref {refObj.reference}, res is {res}")"""
 
-        resp = {"input":prompt,"history":history,"context":retrieved_docs,"answer":resp_answer}
+        #resp = {"input":prompt,"history":history,"context":retrieved_docs,"answer":resp_answer}
         return resp
 
     def runController(self, prompt, history, selected_docs):
 
         print('Selected Docs: ', selected_docs)
-        self.retriever = self.constructRetriever(selected_docs)
+        self.retriever.constructRetriever(db=self.db,selected_docs=selected_docs)
 
         if prompt:
             print(f"Ctrl + C to exit...")
