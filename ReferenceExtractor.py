cb6f77cfa3f6a015bc30dab4d0f13fa5e5851a92
nams1570 <amanganapathy@gmail.com>
2025-05-19 15:21:17 -0700

    Add functionality to extract just docids from string list

--- ReferenceExtractor.py
+++ ReferenceExtractor.py
@@ -68,12 +68,31 @@ class ReferenceExtractor:
                             if len(tup)>0 and tup[-1] != ".":
                                 results.add(tup[0]) 
         return list(results)
+    
+    def extractDocIdsFromStrList(self,str_list:list[str])->list[str]:
+        """@str_list: list of strings which may or may not have docid references in them.
+        Docid is of the form xy.pqr usually and identifies a specific document.
+        returns: list of extracted docids with the handle they are prefaced by"""
+        results = set()
+        patterns = [r"(TS (\d+(.\d+)*))",r"(TR (\d+(.\d+)*))"]
+        extractRegxs = [ re.compile(pattern) for pattern in patterns]
+        
+        for st in str_list:
+            for regx in extractRegxs:
+                temp = regx.findall(st)
+                if temp:
+                    for tup in temp:
+                        if len(tup)>0 and tup[-1] != ".":
+                            results.add(tup[0])
+        return list(results)
 
 
 if __name__ == "__main__":
-    examples = ["The determination of the used resource allocation table is defined in clause 6.1.2.1.1 of [4, TS 38.211] though you can also check Clause 6.2 or clause 6.3.",
+    """examples = ["The determination of the used resource allocation table is defined in clause 6.1.2.1.1 of [4, TS 38.211] though you can also check Clause 6.2 or clause 6.3.",
     "Aperiodic CSI-RS is configured and triggered/activated as described in Clause 8.5.1.2", 
-    "The UE shall derive CQI as specified in clause 5.2.2.1 of [TS, ]", "the UE procedure for receiving the PDSCH upon detection of a PDCCH follows clause 5.1 and the QCL assumption for the PDSCH as defined in clause 5.1.5"]
-    re = ReferenceExtractor()
-    matchedStrings = re.findAllMatches(examples[2])
-    print(re.extractDocumentFromStrings(matchedStrings))
+    "The UE shall derive CQI as specified in clause 5.2.2.1 of [TS, ]", "the UE procedure for receiving the PDSCH upon detection of a PDCCH follows clause 5.1 and the QCL assumption for the PDSCH as defined in clause 5.1.5"]"""
+    ref = ReferenceExtractor()
+    #matchedStrings = re.findAllMatches(examples[2])
+    #print(re.extractDocumentFromStrings(matchedStrings))
+    examples = ["3GPP TS 38.413 V17.2.0 (2022-09)","NGAP is developed in accordance to the general principles stated in TS 38.401 [2] and TS 38.410 [3].","[1] 3GPP TR 21.905: 'Vocabulary for 3GPP Specifications'.","For each PDU session, if the Network Instance IE is included in the PDU Session Resource Setup Request Transfer IE contained in the PDU SESSION RESOURCE SETUP REQUEST message and the Common Network Instance IE is not present, the NG-RAN node shall, if supported, use it when selecting transport network resource as specified in TS 23.501 [9]."]
+    print(ref.extractDocIdsFromStrList(examples))
