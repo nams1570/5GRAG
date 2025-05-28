afd745e876b3c0c8908c4dd2da4db43912ffd5cf
nams1570 <amanganapathy@gmail.com>
2025-05-27 23:49:08 -0700

    Create new SystemModel to represent 5grag controller

--- eval/SystemModels.py
+++ eval/SystemModels.py
@@ -4,6 +4,7 @@ sys.path.append("../")
 from abc import ABC,abstractmethod
 from openai import OpenAI
 from settings import config
+from controller import Controller
 
 class BaseSystemModel(ABC):
     @abstractmethod
@@ -34,8 +35,23 @@ class GPTSystemModel(BaseSystemModel):
             seed=0,
         )
         return response.choices[0].message.content
+    
+class ControllerSystemModel(BaseSystemModel):
+    def __init__(self,doc_dir_path:str,db_dir_path:str,isDBInitialized:bool=False):
+        self.c = Controller(doc_dir_path=doc_dir_path,db_dir_path=db_dir_path)
+        self.isDBInitialized = isDBInitialized
+
+    def get_response(self, question:str):
+        if not self.isDBInitialized:
+            self.c.updateContextDB()
+            self.isDBInitialized = True
+        response,_,_ = self.c.runController(prompt=question,history=[],selected_docs=[])
+        return response
+
 
 if __name__ == "__main__":
     gpt = GPTSystemModel()
-    question = "What does the abbreviation \"IAB\" stand for in the context of NR Integrated access and backhaul?"
-    print(gpt.get_response(question))
+    c = ControllerSystemModel(isDBInitialized=True,doc_dir_path="../data",db_dir_path="../pickles")
+
+    question = "What is spurious response?"
+    print(c.get_response(question))
