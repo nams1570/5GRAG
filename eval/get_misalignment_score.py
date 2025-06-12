62cae93730c3954bf47a369535acf641d7832e67
nams1570 <amanganapathy@gmail.com>
2025-06-11 20:19:44 -0700

    get precision and recall per file based on misalignment
    Note a couple of things:
    for each chunk (which Im defining as a section wide chunk), I get all the references in it\
     using the ReferenceExtractor.
     I'm calling this set the true_refs.
    
    Then, I run the semantic search/ basic retriever using the chunk itself \
    (again defined as a section wide chunk, the same chunk as in step 1) as the query.
    
    Then, I use the ReferenceExtractor to pull out all the refs in the documents from the returned docs.
    We'll called this set retrieved_refs
    I also have a list of all the section names in the document itself.
    Let's call this set all_sections_in_documents.

---
+++ eval/get_misalignment_score.py
@@ -0,0 +1,93 @@
+import sys
+sys.path.append("..")
+from MultiStageRetriever import MultiStageRetriever
+from ReferenceExtractor import ReferenceExtractor
+from langchain_openai import OpenAIEmbeddings
+from MetadataAwareChunker import getSectionedChunks,getFullSectionChunks,clean_file_name
+from DBClient import DBClient
+from settings import config
+from utils import RefObj
+from typing import Tuple
+#get the retriever and client
+
+RE = ReferenceExtractor()
+
+def get_sections_from_docs(docs):
+    sections = set()
+    for doc in docs:
+        sections.add(doc.metadata["section"])
+    return list(sections)
+
+def get_refs_without_tables(refs:list[RefObj])->list[RefObj]:
+    final_refs = []
+    for ref in refs:
+        if "-" not in ref.reference:
+            final_refs.append(ref)
+    return final_refs
+
+def get_chunks_with_refs(docs):
+    """we exclude any table refs. 
+    Returns only those chunks which have intra-doc clause references."""
+    chunks = []
+    chunk_to_refs = {}
+    for chunk in docs:
+        allRefs = get_refs_without_tables(RE.runREWithDocList(docs=[chunk]))
+        section_names = RE.extractClauseNumbersOfSrc(allRefs)
+        if section_names:
+            chunk_to_refs[chunk.page_content] = section_names
+            chunks.append(chunk)
+    return chunks,chunk_to_refs
+
+def get_all_existing_sections(ref_set:set,chunks)->Tuple[set,set]:
+    all_refs_in_doc = set()
+    for chunk in chunks:
+        all_refs_in_doc.add(chunk.metadata["section"])
+    return ref_set.intersection(all_refs_in_doc),all_refs_in_doc
+
+def count_hit_rate_with_retrieval(chunks_in_file,org_chunk,db):
+    """Calculates precision and recall, based on all of the `sections` retrieved by the retriever. 
+    So tp,tn,fp,fn is calculated based on the number of sections not the number of chunks who meet criteria"""
+    true_refs = get_refs_without_tables(RE.runREWithDocList([org_chunk]))
+    true_refs = set(RE.extractClauseNumbersOfSrc(true_refs))
+    print(f"true_refs before:{true_refs}")
+
+    #ensure that only clauses that can be inthe document are accessed
+    true_refs,all_sections_in_org_file = get_all_existing_sections(true_refs,chunks_in_file)
+    print(f"true_refs:{true_refs}, all_refs are {all_sections_in_org_file}")
+    org_docs,_ = mr.invoke(org_chunk.page_content,db)
+    #check the sections of the org_docs.  
+    retriever_refs = get_refs_without_tables(RE.runREWithDocList(org_docs))
+    retriever_refs = set(RE.extractClauseNumbersOfSrc(retriever_refs))
+    print(f"retriever refs: {retriever_refs}")
+
+    tp = len(retriever_refs.intersection(true_refs))
+    fp = len(retriever_refs.difference(true_refs))
+    fn =len(true_refs.difference(retriever_refs))
+    
+    true_complement = all_sections_in_org_file.difference(true_refs)
+    retrieved_complement  = all_sections_in_org_file.difference(retriever_refs)
+    tn = true_complement.intersection(retrieved_complement)
+
+    precision = tp/(tp+fp) if tp+fp !=0 else None
+    recall = tp/(tp+fn) if tp+fn !=0 else None
+    f1_score = (2*precision*recall)/(precision+recall) if precision and recall else None
+
+    return {org_chunk.metadata["section"]:{"precision":precision,"recall":recall,"f1_score":f1_score}}
+
+if __name__ == "__main__":
+    ## Setup classes
+    mr = MultiStageRetriever()
+    embeddings = OpenAIEmbeddings(model='text-embedding-3-large',api_key=config["API_KEY"])
+    db = DBClient(embedding_model=embeddings,db_dir_path="../pickles",doc_dir_path="../data")
+
+    ## for each doc, get chunks and see hit rate
+    file_list = ["../data/38211-i60.docx"]
+    results = {}
+    for file in file_list:
+        mr.constructRetriever(db,selected_docs=[clean_file_name(file)])
+
+        chunks = getFullSectionChunks([file])
+        chunks_with_refs,_ = get_chunks_with_refs(chunks)
+        for chunk_with_ref in chunks_with_refs[:2]:
+            results = {**results,**count_hit_rate_with_retrieval(chunks,chunk_with_ref,db)}
+    print(results)
