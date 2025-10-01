7f5e43c0de8127961123d0b995fe95451ed94439
nams1570 <amanganapathy@gmail.com>
2025-10-01 11:11:31 -0700

    Refactor ReasonDB script to make it easier to run other parsing methods

--- ConstructReasonDB.py
+++ ConstructReasonDB.py
@@ -1,21 +1,33 @@
 from DBClient import DBClient
-from MetadataAwareChunker import getSectionedChunks,addExtraDocumentWideMetadataForReason
+from MetadataAwareChunker import getSectionedChunks,addExtraDocumentWideMetadataForReason, getCRChunks
+from controller import DB_DIR
 from settings import config
 import os
 from langchain_openai import OpenAIEmbeddings
 from utils import convertAllDocToDocx,getAllFilesInDirMatchingFormat
 
-if __name__ == "__main__":
-    embeddings = OpenAIEmbeddings(model='text-embedding-3-large',api_key=config["API_KEY"])
-    db = DBClient(embedding_model=embeddings,collection_name=config["TDOC_COLL_NAME"],db_dir_path=config["CHROMA_DIR"],doc_dir_path="reasoning")
-
-    convertAllDocToDocx("reasoning")
-    file_list = getAllFilesInDirMatchingFormat("reasoning")
-
+def parse_nonCR_docs(file_list,db):
     docs = []
     for file in file_list:
         docs += getSectionedChunks([os.path.join("reasoning",file)],addExtraDocumentWideMetadataForReason)
-
+    
     db.vector_db.add_documents(docs)
-    retriever = db.getRetriever()
+
+def parse_CR_docs(file_list,db):
+    docs = getCRChunks([os.path.join("reasoning",file) for file in file_list])
+    db.vector_db.add_documents(docs)
+
+COLLECTION_NAME = config["TDOC_COLL_NAME"]
+DB_DIR_PATH = config["CHROMA_DIR"]
+DOC_DIR_PATH = "reasoning"
+
+if __name__ == "__main__":
+    embeddings = OpenAIEmbeddings(model='text-embedding-3-large',api_key=config["API_KEY"])
+    db = DBClient(embedding_model=embeddings,collection_name=COLLECTION_NAME,db_dir_path=DB_DIR_PATH,doc_dir_path=DOC_DIR_PATH)
+
+    convertAllDocToDocx(DOC_DIR_PATH)
+    file_list = getAllFilesInDirMatchingFormat(DOC_DIR_PATH)
+
+    #parse_nonCR_docs(file_list,db)
+
     
