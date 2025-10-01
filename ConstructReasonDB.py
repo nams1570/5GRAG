25fc4cb376704dda1053a5dfe2c956b4159733d2
nams1570 <amanganapathy@gmail.com>
2025-10-01 14:07:04 -0700

    changes to reason db construction to allow cr parsing

--- ConstructReasonDB.py
+++ ConstructReasonDB.py
@@ -6,28 +6,29 @@ import os
 from langchain_openai import OpenAIEmbeddings
 from utils import convertAllDocToDocx,getAllFilesInDirMatchingFormat
 
+COLLECTION_NAME =  "specs_and_discussions"
+DB_DIR_PATH = os.path.join("baseline","db")
+DOC_DIR_PATH = os.path.join("./all3gppdocsfromrel17and18","CRs")
+
 def parse_nonCR_docs(file_list,db):
     docs = []
     for file in file_list:
-        docs += getSectionedChunks([os.path.join("reasoning",file)],addExtraDocumentWideMetadataForReason)
+        docs += getSectionedChunks([os.path.join(DOC_DIR_PATH,file)],addExtraDocumentWideMetadataForReason)
     
     db.vector_db.add_documents(docs)
 
 def parse_CR_docs(file_list,db):
-    docs = getCRChunks([os.path.join("reasoning",file) for file in file_list])
+    docs = getCRChunks([os.path.join(DOC_DIR_PATH,file) for file in file_list])
     db.vector_db.add_documents(docs)
-
-COLLECTION_NAME = config["TDOC_COLL_NAME"]
-DB_DIR_PATH = config["CHROMA_DIR"]
-DOC_DIR_PATH = "reasoning"
 
 if __name__ == "__main__":
     embeddings = OpenAIEmbeddings(model='text-embedding-3-large',api_key=config["API_KEY"])
     db = DBClient(embedding_model=embeddings,collection_name=COLLECTION_NAME,db_dir_path=DB_DIR_PATH,doc_dir_path=DOC_DIR_PATH)
 
-    convertAllDocToDocx(DOC_DIR_PATH)
+    #convertAllDocToDocx(DOC_DIR_PATH)
     file_list = getAllFilesInDirMatchingFormat(DOC_DIR_PATH)
-
+    print(file_list)
     #parse_nonCR_docs(file_list,db)
+    parse_CR_docs(file_list,db)
 
     
