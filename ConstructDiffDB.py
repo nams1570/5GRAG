8582dae4e5bd7b63521d2a1af869dc56f71cb512
nams1570 <amanganapathy@gmail.com>
2025-10-03 14:20:52 -0700

    rework diffdb construction to account for docids

--- ConstructDiffDB.py
+++ ConstructDiffDB.py
@@ -1,3 +1,5 @@
+from pydoc import doc
+from webbrowser import get
 from ChangeTracker import ChangeTracker, get_version_preceding_first_in_release, get_doc_list_for_version_preceding_first
 from DBClient import DBClient
 from MetadataAwareChunker import getFullSectionChunks,addExtraDocumentWideMetadataForReason
@@ -8,6 +10,10 @@ from utils import getAllFilesInDirMatchingFormat,convertAllDocToDocx
 
 DIFF_DOC_DIR = "testchange"
 
+def get_sorted_versions(versionMap:dict)->list[str]:
+    versions = sorted(versionMap.keys(), key=lambda v: list(map(int, v.split("."))))
+    return versions
+
 if __name__ == "__main__":
     embeddings = OpenAIEmbeddings(model='text-embedding-3-large',api_key=config["API_KEY"])
     db = DBClient(embedding_model=embeddings,collection_name=config["DIFF_COLL_NAME"],db_dir_path=config["CHROMA_DIR"],doc_dir_path=DIFF_DOC_DIR)
@@ -16,24 +22,38 @@ if __name__ == "__main__":
     file_list = getAllFilesInDirMatchingFormat(DIFF_DOC_DIR)
     print(file_list)
 
-    versionToChunks = {}
+    docIDToversionToChunks = {}
     for file in file_list:
         chunks = getFullSectionChunks([os.path.join(DIFF_DOC_DIR,file)],addExtraDocumentWideMetadataForReason)
 
+        docID = chunks[0].metadata["docID"]
+        if docID not in docIDToversionToChunks:
+            docIDToversionToChunks[docID] = {}
+
         fileVersion = chunks[0].metadata["version"]
-        x,y,z = map(int,chunks[0].metadata["version"].split("."))
+        x,y,z = map(int,fileVersion.split("."))
         releaseNum = x 
         VERSION_PRECEDING_FIRST_IN_RELEASE = get_version_preceding_first_in_release(x)
         if y == 0 and z == 0:
-            versionToChunks[VERSION_PRECEDING_FIRST_IN_RELEASE] = get_doc_list_for_version_preceding_first(chunks[0].metadata,VERSION_PRECEDING_FIRST_IN_RELEASE)
-        versionToChunks[chunks[0].metadata["version"]] = chunks
+            docIDToversionToChunks[docID][VERSION_PRECEDING_FIRST_IN_RELEASE] = get_doc_list_for_version_preceding_first(chunks[0].metadata,VERSION_PRECEDING_FIRST_IN_RELEASE)
+        docIDToversionToChunks[docID][chunks[0].metadata["version"]] = chunks
     
     docs = []
 
-    for fromVersion in versionToChunks.keys():
-        for toVersion in versionToChunks.keys():
-            if ChangeTracker.areAdjacentVersions(fromVersion=fromVersion,toVersion=toVersion):
-                docs.extend(ChangeTracker.createDBDocumentsForAdjacentVersions(versionToChunks[fromVersion],versionToChunks[toVersion]))
+    for docId, versionMap in docIDToversionToChunks.items():
+        versions = get_sorted_versions(versionMap)
+
+        for i in range(len(versions) - 1):
+            fromVersion = versions[i]
+            toVersion = versions[i+1]
+
+            if ChangeTracker.areAdjacentVersions(fromVersion, toVersion):
+                docs.extend(
+                    ChangeTracker.createDBDocumentsForAdjacentVersions(
+                        versionMap[fromVersion],
+                        versionMap[toVersion]
+                    )
+                )
 
     db.vector_db.add_documents(docs)
 
