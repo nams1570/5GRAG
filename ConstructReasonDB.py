ea60d8b5641d68ecbc3756b5c39527d131a8c08c
nams1570 <amanganapathy@gmail.com>
2025-10-04 19:26:16 -0700

    update addition to db for reasondb construction to batch it

--- ConstructReasonDB.py
+++ ConstructReasonDB.py
@@ -3,12 +3,37 @@ from MetadataAwareChunker import getSectionedChunks,addExtraDocumentWideMetadata
 from settings import config
 import os
 from langchain_openai import OpenAIEmbeddings
-from utils import convertAllDocToDocx,getAllFilesInDirMatchingFormat
+from utils import convertAllDocToDocx,getAllFilesInDirMatchingFormat, getTokenCount
 from CollectionNames import REASONING_DOCS
+import time
 
 COLLECTION_NAME =  REASONING_DOCS
 DB_DIR_PATH = os.path.join("baseline","db")
 DOC_DIR_PATH = os.path.join("./all3gppdocsfromrel17and18","CRs")
+
+
+def _safe_add_docs(docs,batch_num,vector_store,attempt=1,max_attempts=3):
+    total_tokens = sum(getTokenCount(d.page_content,model_name="text-embedding-3-large") for d in docs)
+    MAX_TOKENS_ALLOWED = 270000
+
+    if total_tokens > MAX_TOKENS_ALLOWED:
+        print(f"Batch {batch_num} is too large")
+        mid = len(docs) //2
+        _safe_add_docs(docs[:mid],batch_num=f"{batch_num}a",vector_store=vector_store,attempt=attempt,max_attempts=max_attempts)
+        _safe_add_docs(docs[mid:],batch_num=f"{batch_num}b",vector_store=vector_store)
+        return
+    try:
+        vector_store.add_documents(docs)
+        print(f"just added batch {batch_num}")
+    except Exception as e:
+        if attempt < max_attempts:
+            wait = attempt * 30
+            print(f"Error ob batch {batch_num}, attempt {attempt} {e}. Retrying in {wait}s...")
+            time.sleep(wait)
+            _safe_add_docs(docs,batch_num,vector_store=vector_store,attempt=attempt+1,max_attempts=max_attempts)
+        else:
+            print(f"Failed batch {batch_num} after {max_attempts} attempts.")
+            raise e
 
 def parse_nonCR_docs(file_list,db):
     docs = []
@@ -20,7 +45,14 @@ def parse_nonCR_docs(file_list,db):
 def parse_CR_docs(file_list,db):
     docs = getCRChunks([os.path.join(DOC_DIR_PATH,file) for file in file_list])
     print(f"Number of CR chunks is {len(docs)}, now adding to DB")
-    db.vector_db.add_documents(docs)
+
+    batch_size = 1000
+    i=1
+    while (i-1) * batch_size < len(docs):
+        print(f" ****** \n\n number of chunks is {len(docs)} and we are on batch {i}. \n\n")
+        _safe_add_docs(docs[(i-1)*batch_size:i*batch_size],vector_store=db.vector_db,batch_num=i,max_attempts=3)
+        i+=1
+    print(f"Added {len(docs)} chunks to the database")
 
 if __name__ == "__main__":
     embeddings = OpenAIEmbeddings(model='text-embedding-3-large',api_key=config["API_KEY"])
